3747ec6d85a112fc4f13ea10e35eb013a9f987cb
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2026-07-25 16:28:02 +0330

    chore: update example

--- examples/Counter/in-lestin/index.tsx
+++ examples/Counter/in-lestin/index.tsx
@@ -1,10 +1,10 @@
-function Counter({ title, variable }) {
+/*function Counter({ title, variable }) {
 	const Increment = () => {
 		variable++;
 		button.textContent = title + variable;
 	};
 
-	const button: HTMLSpanElement = <button onClick={Increment}></button>;
+	const button = <button onClick={Increment}></button> as HTMLButtonElement;
 	Increment();
 
 	return button;
@@ -15,4 +15,29 @@ document.body.appendChild(
 		<Counter title="Counter One: " variable={-1} />
 		<Counter title="Counter Two: " variable={-1} />
 	</div>,
+);
+
+
+<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>*/
+
+/*let CounterOne = 0,
+	CounterTwo = 0;
+
+document.body.appendChild(
+	<div>
+		<button onClick={function () { this.textContent = "Counter One: " + ++CounterOne; }}>Counter One {CounterOne}</button>
+		<button onClick={function () { this.textContent = "Counter Two: " + ++CounterTwo; }}>Counter Two {CounterTwo}</button>
+	</div>
+);*/
+
+let count = 0;
+
+document.body.appendChild(
+	<button
+		onClick={function () {
+			this.textContent = "count is " + ++count;
+		}}
+	>
+		count is {count}
+	</button>,
 );
